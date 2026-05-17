d826fec1e4e1f887830f9be1a02afeafa27dea31
Alshughriahmed <Alshughriahmed@gmail.com>
2026-05-17 08:47:37 +0200

    chore: export market source contracts

--- bot/market_sources/__init__.py
+++ bot/market_sources/__init__.py
@@ -1,3 +1,18 @@
+from .contracts import (
+    MarketCandle,
+    MarketSourceInfo,
+    ReadOnlyMarketSource,
+    validate_candle,
+    validate_candle_sequence,
+)
 from .registry import MarketSourceStatus, get_market_source_status
 
-__all__ = ["MarketSourceStatus", "get_market_source_status"]
+__all__ = [
+    "MarketCandle",
+    "MarketSourceInfo",
+    "ReadOnlyMarketSource",
+    "validate_candle",
+    "validate_candle_sequence",
+    "MarketSourceStatus",
+    "get_market_source_status",
+]
